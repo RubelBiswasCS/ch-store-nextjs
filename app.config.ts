b528b0b0cc25dd84dfd458101294b5ce888f8dbc
RubelBiswasDH <RubelBiswasDH@gmail.com>
2023-08-09 22:40:12 +0600

    Added custom base query

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,6 @@ export const BASE_URL = 'http://localhost:8000/api'
 
 export const API = {
     LOGIN: `${ BASE_URL }/token/`,
+    REFRESH_TOKEN: `${ BASE_URL }/token/refresh/`,
     PRODUCTS: `${ BASE_URL }/`,
 }
--- redux/services/products.ts
+++ redux/services/products.ts
@@ -1,9 +1,64 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { API } from '@/app.config'
+import type { BaseQueryFn } from '@reduxjs/toolkit/query'
+import axios from 'axios'
+import type { AxiosRequestConfig, AxiosError } from 'axios'
+
+const axiosBaseQuery =
+  (
+    { baseUrl }: { baseUrl: string } = { baseUrl: '' }
+  ): BaseQueryFn<
+    any,
+    unknown,
+    unknown
+  > =>
+  async ({ url, method, data, params, headers, ...rest }) => {
+    console.log({ headers })
+
+    try {
+      const result = await axios({ url: baseUrl + url, method, data, params, headers, ...rest })
+      return { data: result?.data }
+    } catch (axiosError: any) {
+      const status = axiosError?.response?.status ?? null
+      console.log({ status })
+      if(status === 401){
+
+        axios.post(`${ API.REFRESH_TOKEN }`, {
+          refresh: localStorage.getItem('refreshToken'),
+        })
+          .then(async (response) => {
+            const accessToken = response?.data?.access ?? ''
+            const refreshToken = response?.data?.refresh ?? ''
+            localStorage.setItem('accessToken', accessToken)
+            localStorage.setItem('refreshToken', refreshToken)
+            const result = await axios({ 
+              url: baseUrl + url, 
+              method, 
+              data, 
+              params,
+              ...rest,
+              headers: { ...headers, Authorization: `JWT ${accessToken}`  } 
+            })
+            return { data: result?.data }
+          })
+          .catch((err) => {
+            console.log({ err })
+            window.location.href = 'login'
+          })
+      }
+      let err = axiosError as AxiosError
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      }
+    }
+  }
 
 export const productApi = createApi({
   reducerPath: 'productApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '' }),
+  baseQuery: axiosBaseQuery({ baseUrl: '' }),
   endpoints: (builder) => ({
     getProducts: builder.query<any, any>({
       query: () => {
@@ -11,7 +66,7 @@ export const productApi = createApi({
             url: `${ API.PRODUCTS }`,
             method: 'GET',
             headers: {
-                Authorization: `JWT ${ localStorage.getItem('accessToken') }`
+              Authorization: `JWT ${ localStorage.getItem('accessToken') }`
             }
         })
       },
